99013ca020c78cc8ae7ebbdec6229096533920ca
Max Zakharzhevskiy <maxzz@msn.com>
2023-05-14 14:21:33 -0700

    WIP. add Section2_Main skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { Section2_Main } from "./components/section2_main";
+
 export function App() {
     return (
         <div className="h-screen bg-green-50">
-            Now
+            <Section2_Main />
         </div>
     );
 }
---
+++ src/components/section2_main/index.tsx
@@ -0,0 +1,7 @@
+export function Section2_Main() {
+    return (
+        <div className="p-4 text-slate-800">
+            <button className="px-3 py-1.5 bg-slate-100 border-state-300 border rounded shadow">Start</button>
+        </div>
+    );
+}
