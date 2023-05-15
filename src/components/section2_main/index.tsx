bbfa3e949c1b4402f9b10f4b9552bd4a76f9e4f6
Max Zakharzhevskiy <maxzz@msn.com>
2023-05-14 18:01:55 -0700

    WIP. Section2_Main. add test-authentication-session

--- src/components/section2_main/index.tsx
+++ src/components/section2_main/index.tsx
@@ -1,9 +1,11 @@
-import { processNow } from "../../srp-tests";
+import { testAuthenticationSession } from "../../srp-tests";
 
 export function Section2_Main() {
     return (
         <div className="p-4 text-slate-800">
-            <button className="px-3 py-1.5 bg-slate-100 border-state-300 border rounded shadow" onClick={async () => { processNow(); }}>Start</button>
+            <button className="px-3 py-1.5 bg-slate-100 border-state-300 border rounded shadow" onClick={async () => { testAuthenticationSession(); }}>
+                Start
+            </button>
         </div>
     );
 }
