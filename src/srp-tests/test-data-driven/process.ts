e66311de6fb7bf2a2eaad2dffeb8f0b504531963
Max Zakharzhevskiy <maxzz@msn.com>
2023-05-15 17:24:38 -0700

    WIP. DataDriven. add ProcessItem[]

--- src/srp-tests/test-data-driven/process.ts
+++ src/srp-tests/test-data-driven/process.ts
@@ -15,6 +15,7 @@ import { steps } from './steps-data';
 export type ProcessItem = {
     className: string;
     text: string;
+    items?: ProcessItem;
 }
 
 export const PUPPETEER_PROMISE = new Promise(async (resolve) => {
@@ -23,14 +24,19 @@ export const PUPPETEER_PROMISE = new Promise(async (resolve) => {
 
     let TEST_PASSED = null;
 
+    const processItems: ProcessItem[] = [];
+
     const resultsAcc: Record<string, any> = {};
 
     for (const step of steps) {
         if (step.end) {
-            const stepDivElm = document.createElement("div");
-            stepDivElm.className = `code code-${step.end}`;
-            stepDivElm.innerHTML = step.code || '';
-            sequenceElm.appendChild(stepDivElm);
+
+            processItems.push({className: `code code-${step.end}`, text: step.code || ''});
+
+            // const stepDivElm = document.createElement("div");
+            // stepDivElm.className = `code code-${step.end}`;
+            // stepDivElm.innerHTML = step.code || '';
+            // sequenceElm.appendChild(stepDivElm);
 
             if (!step.fakecode) {
                 try {
@@ -38,32 +44,42 @@ export const PUPPETEER_PROMISE = new Promise(async (resolve) => {
 
                     let str = JSON.stringify(result, null, "  ");
                     if (typeof str === "string") {
-                        const valueDiv = document.createElement("div");
-                        valueDiv.className = "codevalue";
-                        valueDiv.innerText += `> ${step.return}: ` + str;
-                        stepDivElm.appendChild(valueDiv);
+
+                        processItems.push({className: 'codevalue', text: `> ${step.return}: ` + str});
+
+                        // const valueDiv = document.createElement("div");
+                        // valueDiv.className = "codevalue";
+                        // valueDiv.innerText += `> ${step.return}: ` + str;
+                        // stepDivElm.appendChild(valueDiv);
                     }
                     
                     if (result && step.return) {
                         resultsAcc[step.return] = result;
                     }
                 } catch (error) {
-                    stepDivElm.innerText += `\n> ${(error as Error).toString()}`;
-                    stepDivElm.className += " code-error";
+                    processItems.push({className: 'code-error', text: `\n> ${(error as Error).toString()}`});
+
+                    // stepDivElm.innerText += `\n> ${(error as Error).toString()}`;
+                    // stepDivElm.className += " code-error";
+
                     console.error(error);
                     TEST_PASSED = false;
                 }
             }
         } else if (step.request || step.reply) {
-            const div = document.createElement("div");
-            div.className = `comms-${step.request ? "right" : "left"}`;
-            div.innerText = `${step.request || step.reply}(${(step.send || []).join(", ")})`;
-            sequenceElm.appendChild(div);
+            processItems.push({className: `comms-${step.request ? "right" : "left"}`, text: `${step.request || step.reply}(${(step.send || []).join(", ")})`});
+
+            // const div = document.createElement("div");
+            // div.className = `comms-${step.request ? "right" : "left"}`;
+            // div.innerText = `${step.request || step.reply}(${(step.send || []).join(", ")})`;
+            // sequenceElm.appendChild(div);
         } else if (step.break) {
-            const div = document.createElement("div");
-            div.className = "break";
-            div.innerText = `${step.break}`;
-            sequenceElm.appendChild(div);
+            processItems.push({className: 'break', text: `${step.break}`});
+
+            // const div = document.createElement("div");
+            // div.className = "break";
+            // div.innerText = `${step.break}`;
+            // sequenceElm.appendChild(div);
         }
     }
 
