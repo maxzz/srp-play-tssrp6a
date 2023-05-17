3c298df9cb276414d0d6c6b4a99a5d48799592c9
Max Zakharzhevskiy <maxzz@msn.com>
2023-05-17 15:09:31 -0700

    WIP. tailwind. add custom plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,61 @@
+const plugin = require("tailwindcss/plugin");
+const colors = require('tailwindcss/colors');
+
+const customAdditionsPlugin = plugin(function ({ addComponents }) {
+    const newComponents = {
+        ".scrollbar-w-2::-webkit-scrollbar": {
+            width: ".5rem !important",
+            height: ".5rem !important",
+        },
+        ".scrollbar-thumb-rounded::-webkit-scrollbar-thumb": {
+            borderRadius: ".25rem !important",
+        },
+        ".scrollbar-thumb-gray::-webkit-scrollbar-thumb": {
+            backgroundColor: "rgba(156, 163, 175, 1) !important",
+        },
+        ".scrollbar-track-gray-lighter::-webkit-scrollbar-track": {
+            backgroundColor: "rgba(209, 213, 219, 1) !important",
+        },
+        ".scrollbar-thumb-transparent::-webkit-scrollbar-thumb": {
+            backgroundColor: "transparent",
+        },
+        ".scrollbar-track-transparent::-webkit-scrollbar-track": {
+            backgroundColor: "transparent",
+        },
+    };
+    addComponents(newComponents, {
+        variants: ["responsive"],
+    });
+});
+
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ['./index.html', './src/**/*.{tsx,ts}'],
+    darkMode: 'class',
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: colors.slate,
+                // ui: {
+                //     bg: colors.slate[100],
+                //     text: '#5a3543',
+                // },
+            },
+        },
     },
-    plugins: [],
-}
+    plugins: [
+        customAdditionsPlugin,
+        require('./tailwind/tailwnid-plugin-debug-styles'),
+        require('./tailwind/tailwind-plugin-debug-screens'),
+        require('./tailwind/tailwind-plugin-colors-bridge')([
+            { prefix: '--tm-', groupName: 'primary' },
+            // { prefix: '--tm-', groupName: 'ui', groupNameOut: 'ui' },
+        ]),
+        require('./tailwind/tailwind-plugin-overflow-overlay'),
+        require('./tailwind/tailwind-plugin-data-state'),
+        require('tailwindcss-radix')(),
+        require('@tailwindcss/forms')({ strategy: 'class' }),
 
+        require('@tailwindcss/container-queries'),
+    ],
+}
