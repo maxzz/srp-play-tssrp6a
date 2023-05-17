4f6abf491b08462ca97f0e28228178140e7892c4
Max Zakharzhevskiy <maxzz@msn.com>
2023-05-17 14:54:54 -0700

    WIP. store. add client and server databases

--- src/store/srp/index.ts
+++ src/store/srp/index.ts
@@ -1,1 +1,2 @@
 export * from './srp-init';
+export * from './srp-db';
--- src/store/srp/srp-db.ts
+++ src/store/srp/srp-db.ts
@@ -1,8 +1,37 @@
 import { proxy } from "valtio";
+import { uuid } from "../../utils";
 
-export type User = {
+// Client
+
+export type ClientUser = {
+    name: string;
+    password: string;
+
+    uuid: number; // local data for React UI only
+};
+
+const initialClientUsersDb: ClientUser[] = [
+    {
+        name: 'max',
+        password: 'pass',
+
+        uuid: uuid.asRelativeNumber(),
+    },
+    {
+        name: 'max jr.',
+        password: 'pass jr.',
+
+        uuid: uuid.asRelativeNumber(),
+    },
+];
+
+export const clientUsersDb = proxy(initialClientUsersDb);
+
+// Server
+
+export type ServerUser = {
     salt: bigint;
     verifier: bigint;
 };
 
-export const usersDB = proxy<User[]>([]);
+export const serverUsersDb = proxy<ServerUser[]>([]);
