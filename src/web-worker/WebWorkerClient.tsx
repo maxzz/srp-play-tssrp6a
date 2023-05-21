3746d7610878f6e76fb87e3ecd9881bb78d35126
Max Zakharzhevskiy <maxzz@msn.com>
2023-05-21 15:45:47 -0700

    WIP. worker. worker as atom only

--- src/web-worker/WebWorkerClient.tsx
+++ src/web-worker/WebWorkerClient.tsx
@@ -1,73 +1,30 @@
-import { useEffect, useMemo } from 'react';
-import { useAtom } from 'jotai';
+import { useEffect } from 'react';
+import { useAtom, useSetAtom } from 'jotai';
 import { workerAtom } from '@/store';
-import { buttonClasses } from '@/components/section2_main';
 
 function createWorker() {
     return new Worker(new URL('./web-worker-body.ts', import.meta.url));
 }
 
 export function WebWorkerClient() {
-    const [worker, setWorker] = useAtom(workerAtom);
+    const setWorker = useSetAtom(workerAtom);
 
     useEffect(() => {
-        const w = createWorker();
+        const worker = createWorker();
 
-        w.postMessage('client: client started');
+        worker.postMessage('client: client started');
 
-        w.onmessage = (e: MessageEvent<string>) => {
+        worker.onmessage = (e: MessageEvent<string>) => {
             console.log('client: message from worker', e);
         };
 
-        setWorker(w);
+        setWorker(worker);
 
         return () => {
-            w.terminate();
+            worker.terminate();
             setWorker(null);
         };
     }, []);
 
-    function onWorkerClick() {
-        setWorker((prev) => {
-            if (prev) {
-                prev.terminate();
-                return null;
-            } else {
-                const w = createWorker();
-
-                w.postMessage('client2: client started');
-
-                w.onmessage = (e: MessageEvent<string>) => {
-                    console.log('client2: message from worker', e);
-                };
-
-                return w;
-            }
-        });
-    }
-
-    return (
-        <div>
-            <button className={buttonClasses} onClick={onWorkerClick}>{worker ? 'Stop' : 'Start'}</button>
-        </div>
-    );
-}
-
-export function WebWorkerClient2() {
-    const worker = useMemo(() => {
-        return new Worker(new URL('./web-worker-body.ts', import.meta.url));
-    }, []);
-
-    useEffect(() => {
-        if (worker && window.Worker) {
-            worker.postMessage('client started');
-
-            worker.onmessage = (e: MessageEvent<string>) => {
-                console.log('from worker', e);
-            };
-        }
-    }, [worker]);
-    return (
-        <div>WebWorkerClient</div>
-    );
+    return null;
 }
