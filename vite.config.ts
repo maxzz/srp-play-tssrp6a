97d0bed1cb0b0dc2feb69b75d243e119728c1e56
Max Zakharzhevskiy <maxzz@msn.com>
2023-05-25 15:29:07 -0700

    WIP. worker. problem w/ crypto in worker solved w/ node-stdlib-browser

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import nodePolyfills from 'vite-plugin-node-stdlib-browser'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     base: '',
     plugins: [
+        nodePolyfills(),
         react()
     ],
     resolve: {
