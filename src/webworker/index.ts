d73a834bee77dd3d9a184a2920d4949d25a06d82
Max Zakharzhevskiy <maxzz@msn.com>
2023-05-25 15:31:04 -0700

    WIP. worker. ---------- remove Buffer polyfill

--- src/webworker/index.ts
+++ src/webworker/index.ts
@@ -1,6 +1,3 @@
 import { onServerMessages } from './webworker-server-handlers';
 
-import { Buffer } from "buffer";
-globalThis.Buffer = Buffer;
-
 self.addEventListener('message', onServerMessages);
