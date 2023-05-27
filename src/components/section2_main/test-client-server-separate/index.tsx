dccdc19c9914db2764bd7ee48cb4ca3fbae0b9fd
Max Zakharzhevskiy <maxzz@msn.com>
2023-05-26 19:09:19 -0700

    WIP. section2. add create new user function

--- src/components/section2_main/test-client-server-separate/index.tsx
+++ src/components/section2_main/test-client-server-separate/index.tsx
@@ -1,27 +1,27 @@
 import { GridRows } from "./Rows";
 import { classNames } from "@/utils";
 import { buttonClasses } from "..";
-import { IconAdd, IconAddUser, IconLoggedIn, IconLoggedOut } from "@/components/ui";
+import { IconAddUser } from "@/components/ui";
+import { appUi, createNewLoginRow } from "@/store";
 
 function AddRowButton() {
     return (
-        <button className={classNames("px-1 py-1.5", buttonClasses)}>
-            <IconAddUser className="w-4 h-4" />
-            {/* <IconAdd className="w-3 h-3" /> */}
+        <button className={classNames("!px-2", buttonClasses)} title="Add user" onClick={() => {
+            appUi.dataState.client.db.push(createNewLoginRow({ username: 'new name', password: 'new password' }));
+        }}>
+            <IconAddUser className="w-5 h-5" />
         </button>
     );
 }
 
 export function ClientServerSeparate() {
-    async function run() {
-    }
     return (
         <div className="max-w-[64ch] p-4 text-sm space-y-4">
-            <button className={buttonClasses} onClick={async () => { run(); }}>
-                Start
-            </button>
+            <div className="flex items-center justify-between">
+                <span>Clients</span>
+                <AddRowButton />
+            </div>
 
-            <div className="flex items-center space-x-2"><span>Clients</span> <AddRowButton /></div>
             <GridRows />
         </div>
     );
