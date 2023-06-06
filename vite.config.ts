ac2ea3734bc8e5879d6f34ebdb67e901d64b49b9
Max Zakharzhevskiy <maxzz@msn.com>
2023-06-06 12:58:41 -0700

    WIP. app. add basic replace plugin for bundle

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,18 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import nodePolyfills from 'vite-plugin-node-stdlib-browser';
 import visualizer from 'rollup-plugin-visualizer';
+//import replace from '@rollup/plugin-replace';
+
+const buildAt = () => {
+    const d = new Date();
+    const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'long', day: 'numeric' };
+    return d.toLocaleDateString('en-US', options);
+};
+
+const buildVersion = () => {
+    const d = new Date();
+    return `${d.getFullYear().toString().substring(3)}.${d.getMonth() + 1}${d.getDate()} (${d.getHours()}${d.getMinutes()})`;
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +22,13 @@ export default defineConfig({
     plugins: [
         nodePolyfills(),
         react(),
+        // replace({
+        //     values: {
+        //         __BUILD_DATE__: buildAt(),
+        //         __BUILD_VER__: buildVersion(),
+        //     },
+        //     preventAssignment: true,
+        // }),
         visualizer({
             filename: 'visualization.html',
             template: 'sunburst', // sunburst - d3 style (good as default as well); treemap - table (default); network - graph (slow to open).
